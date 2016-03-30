71877173aef71900ad6e08a23c59eb2da8cdc8b4
BastiTee <basti.tee@gmx.de>
2016-03-30 17:04:18 +0200

    little refac;

--- scan_pages_and_create_pdf.py
+++ scan_pages_and_create_pdf.py
@@ -15,31 +15,7 @@ import time
 from reportlab.lib.units import cm
 from reportlab.pdfgen import canvas
 
-
-def convert_images_to_a4_pdf (images, target_pdf='output.pdf'):  
-    """Takes a list of JPG/PNG/BMP images and stores the images to 
-       an A4-format PDF with each image as one page."""
-       
-    c = canvas.Canvas(target_pdf)
-    c.setPageCompression(1)
-    for image in images:
-        c.drawImage(image, 0, 0, 21 * cm, 29.7 * cm)
-        c.showPage()
-    c.save()
-    
-
-def scan_image (target_file, resolution=100, contrast=0):
-    """Uses a twain-compatible scanner to scan an image to BMP"""
-    
-    workdir = path.join(path.dirname(path.realpath(__file__)),
-                        'cmdtwain-win')
-    command = '{} /PAPER=a4 /RGB /DPI={} {}'.format(
-                path.join(workdir, 'ScanBmp.exe'), resolution, target_file)
-    
-    handle = Popen(command, shell=True, stdout=PIPE,
-                              stderr=STDOUT, cwd=workdir)
-    handle.wait()
-
+# Invoke GUI
 Tkinter.Tk().withdraw()
 
 # Command line arguments
@@ -54,7 +30,8 @@ parser.add_argument('-keeptemp', action='store_true',
 args = parser.parse_args()
     
 # Get target_filename for final PDF file
-default_filename = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d') + '-DOCNAME'
+default_filename = (datetime.fromtimestamp(
+                    time.time()).strftime('%Y-%m-%d') + '-DOCNAME')
 file_opt = {}
 file_opt['initialdir'] = '.'
 file_opt['initialfile'] = default_filename
@@ -74,6 +51,8 @@ target_folder = path.abspath(path.join(target_filename, pardir))
 print 'Target folder for temporary files will be {0}'.format(target_folder)
 
 images = []
+workdir = path.join(path.dirname(path.realpath(__file__)),
+                        'cmdtwain-win')
 
 # Scan as many pages as the user desires...
 while True:
@@ -82,7 +61,13 @@ while True:
     image = path.join(target_folder, timestamp) + '.bmp'
     print 'Creating temporary file at: {0}'.format(image)
     
-    scan_image (image, args.r, args.c)
+    
+    command = '{} /PAPER=a4 /RGB /DPI={} {}'.format(
+                path.join(workdir, 'ScanBmp.exe'), args.r, image)
+    
+    handle = Popen(command, shell=True, stdout=PIPE,
+                              stderr=STDOUT, cwd=workdir)
+    handle.wait()
     images.append(image)
     
     # Now the temporary image file lies on the file system
@@ -94,7 +79,14 @@ while True:
     else:
         break
 
-convert_images_to_a4_pdf(images, target_filename)
+# Take a list of JPG/PNG/BMP images and stores the images to 
+# an A4-format PDF with each image as one page
+c = canvas.Canvas(target_filename)
+c.setPageCompression(1)
+for image in images:
+    c.drawImage(image, 0, 0, 21 * cm, 29.7 * cm)
+    c.showPage()
+c.save()
 
 if args.keeptemp == 'False':
     for temp_file in images:
