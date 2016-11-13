ac4581925f23d840c9de8608452577fad4f18c98
Basti Tee <basti.tee@gmx.de>
2016-11-13 13:05:06 +0100

    added linux suppot;

--- scan_pages_and_create_pdf.py
+++ scan_pages_and_create_pdf.py
@@ -22,10 +22,9 @@ else:
     mb = messagebox
     fd = filedialog
     tk = Tk
-
 from argparse import ArgumentParser
 from datetime import datetime
-from os import path, remove, pardir
+from os import path, remove, pardir, devnull, errno
 from subprocess import PIPE, STDOUT, Popen
 import time
 from PIL import Image
@@ -34,98 +33,110 @@ from reportlab.pdfgen import canvas
 from platform import system
 
 system_string =  str(system()).lower()
+print ("Running OS: {}".format(system_string))
+
+# check if scanimage is installed on linux 
+if "linux" in system_string:
+    try:
+        dnu = open(devnull)
+        Popen(["scanimage", "-V"], stdout=dnu, stderr=dnu).communicate()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print ("'scanimage' not available. Will abort.")
+            exit(1)
 
 # Invoke GUI
 tk().withdraw()
 
 # Command line arguments
-parser = ArgumentParser(description='Scan pages and create PDF.')
-parser.add_argument('-r', metavar='<Resolution>', default=100,
-                    help='Scan resolution in DPI (100 - 1000).')
-parser.add_argument('-c', metavar='<Contrast>', default=0,
-                    help='Contrast (-1000 - 1000).')
-parser.add_argument('-i', metavar='<Image-Qlt>', default=90,
-                    help='Image Quality (10-100).')
-parser.add_argument('-k', action='store_true',
-                    help='Keep temporary image files', default='False')
+parser = ArgumentParser(description="Scan pages and create PDF.")
+parser.add_argument("-r", metavar="<Resolution>", default=300,
+                    help="Scan resolution in DPI (100 - 1000).")
+parser.add_argument("-c", metavar="<Contrast>", default=0,
+                    help="Contrast (-1000 - 1000).")
+parser.add_argument("-i", metavar="<Image-Qlt>", default=50,
+                    help="Image Quality (10-100).")
+parser.add_argument("-k", action="store_true",
+                    help="Keep temporary image files", default="False")
 
 args = parser.parse_args()
 
 args.r = int(args.r)
 if args.r < 100 or args.r > 1000:
-    print ('Value for resolution ({}) invalid.'.format(args.r))
+    print ("Value for resolution ({}) invalid.".format(args.r))
     parser.print_help()
     exit(1)
 
 args.c = int(args.c)
 if args.c < -1000 or args.c > 1000:
-    print ('Value for contrasts ({}) invalid.'.format(args.c))
+    print ("Value for contrasts ({}) invalid.".format(args.c))
     parser.print_help()
     exit(1)
 
 args.i = int(args.i)
 if args.i < 10 or args.i > 100:
-    print ('Value for jpeg quality ({}) invalid.'.format(args.i))
+    print ("Value for jpeg quality ({}) invalid.".format(args.i))
     parser.print_help()
     exit(1)
 
-print ('SCAN_RES={}; CONTRAST={}; JPEG_Q={}; KEEP_TMP={}'.format(
+print ("SCAN_RES={}; CONTRAST={}; JPEG_Q={}; KEEP_TMP={}".format(
     args.r, args.c, args.i, args.k))
 
 # Get target_filename for final PDF file
 default_filename = (datetime.fromtimestamp(
-                    time.time()).strftime('%Y-%m-%d') + '-DOCNAME')
+                    time.time()).strftime("%Y-%m-%d") + "-DOCNAME")
 file_opt = {}
-file_opt['initialdir'] = '.'
-file_opt['initialfile'] = default_filename
-file_opt['title'] = 'Select location for final PDF file'
-file_opt['filetypes'] = [('PDF Files', '.pdf')]
+file_opt["initialdir"] = "."
+file_opt["initialfile"] = default_filename
+file_opt["title"] = "Select location for final PDF file"
+file_opt["filetypes"] = [("PDF Files", ".pdf")]
 target_filename = fd.asksaveasfilename(**file_opt)
 
-if target_filename == '':
+if target_filename == "":
     exit()
 else:
-    if not target_filename.endswith('.pdf'):
-        target_filename = target_filename + '.pdf'
+    if not target_filename.endswith(".pdf"):
+        target_filename = target_filename + ".pdf"
 
 target_filename = path.abspath(target_filename)
-print ('Target filename will be {0}'.format(target_filename))
+print ("Target filename will be {0}".format(target_filename))
 target_folder = path.abspath(path.join(target_filename, pardir))
-print ('Target folder for temporary files will be {0}'.format(target_folder))
+print ("Target folder for temporary files will be {0}".format(target_folder))
 
 images = []
 temp_files = []
 workdir = path.join(path.dirname(path.realpath(__file__)),
-                        'cmdtwain-win')
+                        "cmdtwain-win")
 
 # Scan as many pages as the user desires...
 while True:
 
-    timestamp = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H%M%S')
-    image = path.join(target_folder, timestamp) + '.bmp'
-    print ('Creating temporary file at: {0}'.format(image))
+    timestamp = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H%M%S")
 
-    if system_string is 'windows':
-        command = '{} /PAPER=a4 /RGB /DPI={} \"{}\"'.format(
-                    path.join(workdir, 'ScanBmp.exe'), args.r, image)
+    image = path.join(target_folder, timestamp)
+    print ("Creating temporary file at: {0}".format(image))
+
+    if "windows" in system_string:
+        image = image + ".bmp"
+        command = "{} /PAPER=a4 /RGB /DPI={} \"{}\"".format(
+                    path.join(workdir, "ScanBmp.exe"), args.r, image)
 
         handle = Popen(command, shell=True, stdout=PIPE,
                                   stderr=STDOUT, cwd=workdir)
         handle.wait()
-    elif system_string is 'linux':
-        # command = '{} --format=tiff > \"{}\"'.format(
-        #             'scanimage'), image)
-        # handle = Popen(command, shell=True, stdout=PIPE,
-        #                           stderr=STDOUT, cwd=workdir)
-        # handle.wait()
-        print ( "Support for operating system coming soon. Will abort.");
-        break
+    elif "linux" in system_string:
+        image = image + ".jpg"
+        command = "{} --resolution {} --format=jpeg > \"{}\"".format(
+                     "scanimage", args.r, image)
+        handle = Popen(command, shell=True, stdout=PIPE,
+                                   stderr=STDOUT, cwd=workdir)
+        handle.wait()
     else:
         print ( "Unsupported operating system. Will abort.");
         break
 
     if not path.exists(image):
-        print ('Creation of file seems to have failed. Will abort.')
+        print ("Creation of file seems to have failed. Will abort.")
         break
 
     images.append(image)
@@ -135,13 +146,13 @@ while True:
 
     result = mb.askquestion(title="Basti's scan tool",
                                        message="Scan another page?")
-    if result == 'yes':
+    if result == "yes":
         pass
     else:
         break
 
 if len(images) == 0:
-    print ('No pages scanned. PDF will not be created.')
+    print ("No pages scanned. PDF will not be created.")
     exit()
 
 # Take a list of JPG/PNG/BMP images and stores the images to
@@ -149,19 +160,19 @@ if len(images) == 0:
 c = canvas.Canvas(target_filename)
 c.setPageCompression(1)
 for image in images:
-    image_jpeg = image + '.jpg'
+    image_jpeg = image + ".q.jpg"
     temp_files.append(image_jpeg)
     try:
         im = Image.open(image)
-        im.save(image_jpeg, 'JPEG', quality=args.i)
+        im.save(image_jpeg, "JPEG", quality=args.i)
     except IOError:
-        print ('Cannot create jpeg for {}'.format(image))
+        print ("Cannot create jpeg for {}".format(image))
     c.drawImage(image_jpeg, 0, 0, 21 * cm, 29.7 * cm)
     c.showPage()
 c.save()
 
-if args.k == 'False':
+if args.k == "False":
     for temp_file in temp_files:
         if path.exists(temp_file):
-            print ('Removing temporary file: {0}'.format(temp_file))
+            print ("Removing temporary file: {0}".format(temp_file))
             remove(temp_file)
